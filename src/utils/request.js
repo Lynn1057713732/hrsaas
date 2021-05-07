0122f55722b1150739bffa36cc70c82b1cd5823f
hector <sheep123_yang@163.com>
2021-05-07 11:48:38 +0800

    前端主动检查token是否超时以及解决后端返回token超时情况下的登出操作

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,6 +2,9 @@ import axios from 'axios'
 import store from '@/store'
 import router from '@/router'
 import { Message } from 'element-ui'
+import { getTimeStamp } from '@/utils/auth'
+
+const TimeOut = 3600 // 定义token的超时时间，用于前端主动检查token是否超时
 
 const service = axios.create({
   // 当执行 npm run dev  => .evn.development => /api => 跨域代理
@@ -13,6 +16,15 @@ const service = axios.create({
 service.interceptors.request.use(config => {
   // 在这个位置需要统一的去注入token
   if (store.getters.token) {
+    // 只有在有token的情况下 才有必要去检查时间戳是否超时
+    if (IsCheckTimeOut()) {
+      // 如果它为true表示 过期了
+      // token没用了 因为超时了
+      store.dispatch('user/logout') // 登出操作
+      // 跳转到登录页
+      router.push('/login')
+      return Promise.reject(new Error('token超时了'))
+    }
     // 如果token存在 注入token
     config.headers['Authorization'] = `Bearer ${store.getters.token}`
   }
@@ -35,6 +47,7 @@ service.interceptors.response.use(response => {
   }
 }, error => {
   // error 信息 里面 response的对象
+  // 当error code从后端返回token过期或者出错的状态码，就直接登出。
   if (error.response && error.response.data && error.response.data.code === 10002) {
     // 当等于10002的时候 表示 后端告诉我token超时了
     store.dispatch('user/logout') // 登出action 删除token
@@ -44,5 +57,13 @@ service.interceptors.response.use(response => {
   }
   return Promise.reject(error)
 })
+
+// 前端主动检车token是否超时
+// 超时逻辑  (当前时间  - 缓存中的时间) 是否大于 时间差
+function IsCheckTimeOut() {
+  var currentTime = Date.now() // 当前时间戳
+  var timeStamp = getTimeStamp() // 缓存时间戳
+  return (currentTime - timeStamp) / 1000 > TimeOut
+}
 
 export default service
