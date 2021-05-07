e51570538e96671dbc6e4ce0a5cd4896eecac555
hector <sheep123_yang@163.com>
2021-05-07 09:36:29 +0800

    token的拦截和跳转处理

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -120,7 +120,8 @@ export default {
           try {
             this.loading = true
             // 只有校验通过了 我们才去调用action
-            await this['user/login'](this.loginForm)
+            // await this['user/login'](this.loginForm)  //两种方法皆可
+            await this.$store.dispatch('user/login', this.loginForm)
             // 应该登录成功之后
             // async标记的函数实际上一个promise对象
             // await下面的代码 都是成功执行的代码
