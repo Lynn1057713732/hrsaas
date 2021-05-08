ed7fa95b6a111e5999c14e3f63fae6e01c54b6ff
hector <sheep123_yang@163.com>
2021-05-08 21:29:55 +0800

    公司设置页面的功能完成

--- src/store/getters.js
+++ src/store/getters.js
@@ -4,6 +4,7 @@ const getters = {
   token: state => state.user.token, // 在根级的getters上 开发子模块的属性给别人看 给别人用
   avatar: state => state.user.userInfo.staffPhoto, // 建立用户头像的映射,用于快捷访问
   name: state => state.user.userInfo.username, // 建立用户username的映射,用于快捷访问
-  userId: state => state.user.userInfo.userId // 建立用户id的映射,用于快捷访问,用于快捷访问
+  userId: state => state.user.userInfo.userId, // 建立用户id的映射,用于快捷访问,用于快捷访问
+  companyId: state => state.user.userInfo.companyId // 建立对于user模块的companyId的快捷访问
 }
 export default getters
