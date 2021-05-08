4006810066768f0ce9c82771a984d8a183d5e14e
hector <sheep123_yang@163.com>
2021-05-08 08:39:23 +0800

    完成组织架构的功能

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,20 +1,81 @@
 <template>
-  <div class="dashboard-container">
+  <div v-loading="loading" class="dashboard-container">
     <div class="app-container">
-      <h2>
-        组织架构
-      </h2>
+      <!-- 实现页面的基本布局 -->
+      <el-card class="tree-card">
+        <!-- 用了一个行列布局 -->
+        <!-- 缺少treeNode -->
+        <tree-tools :tree-node="company" :is-root="true" @addDepts="addDepts" />
+        <!--放置一个属性   这里的props和我们之前学习的父传子 的props没关系-->
+        <el-tree :data="departs" :props="defaultProps" default-expand-all>
+          <!-- 说明el-tree里面的这个内容 就是插槽内容 => 填坑内容  => 有多少个节点循环多少次 -->
+          <!-- scope-scope 是 tree组件传给每个节点的插槽的内容的数据 -->
+          <!-- 顺序一定是 执行slot-scope的赋值 才去执行 props的传值 -->
+          <tree-tools slot-scope="obj" :tree-node="obj.data" @addDepts="addDepts" @editDepts="editDepts" @delDepts="getDepartments" />
+        </el-tree>
+      </el-card>
     </div>
+
+    <!-- 放置新增弹层组件 -->
+    <add-dept ref="addDept" :show-dialog.sync="showDialog" :tree-node="node" @addDepts="getDepartments" />
   </div>
 </template>
 
 <script>
-export default {
+import TreeTools from './components/tree-tools'
+import AddDept from './components/add-dept'
 
+import { getDepartments } from '@/api/departments'
+import { tranListToTreeData } from '@/utils'
+export default {
+  components: {
+    TreeTools, AddDept
+  },
+  data() {
+    return {
+      company: { }, // 就是头部的数据结构
+      departs: [],
+      defaultProps: {
+        label: 'name', // 表示 从这个属性显示内容
+        children: 'children'
+      },
+      showDialog: false, // 默认不显示弹层
+      node: null, // 记录当前点击的node节点，用于对话框中的校验
+      loading: false // 当前的控制显示弹层
+    }
+  },
+  created() {
+    this.getDepartments() // 调用自身的方法
+  },
+  methods: {
+    async getDepartments() {
+      this.loading = true
+      const result = await getDepartments()
+      this.company = { name: result.companyName, manager: '负责人', id: '' }
+      this.departs = tranListToTreeData(result.depts, '') // 需要将其转化成树形结构
+      console.log(result)
+      this.loading = false
+    },
+    // 监听tree-tools中触发的点击添加子部门的事件
+    // node 是我们点击的部门
+    addDepts(node) {
+      this.showDialog = true // 显示弹层
+      this.node = node
+    },
+    editDepts(node) {
+      this.showDialog = true // 弹出层
+      this.node = node
+      // 应该在这里调用获取子组件中的部门详情的方法
+      this.$refs.addDept.getDepartDetail(node.id)
+    }
+
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+.tree-card {
+  padding: 30px  140px;
+  font-size:14px;
+}
 </style>
-
