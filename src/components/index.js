d698bd8b922f8bad2e1010acc2619591046300d8
hector <sheep123_yang@163.com>
2021-05-09 20:09:10 +0800

    用户的详情查看，信息修改，头像证件照的上传和展示，个人信息的打印

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,14 @@
 // 负责所有全局自定义组件的注册
 import PageTools from './PageTools/index'
 import UploadExcel from './UploadExcel/index'
+import ImageUpload from './ImageUpload/index'
+import Print from 'vue-print-nb'
+
 export default {
   install(Vue) {
     Vue.component('PageTools', PageTools) // 注册工具栏组件
     Vue.component('UploadExcel', UploadExcel) // 注册导入excel组件
+    Vue.component('ImageUpload', ImageUpload) // 注册图片上传组件
+    Vue.use(Print)
   }
 }
